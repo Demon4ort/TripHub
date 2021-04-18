a1929ba63801bf0976d5cb80ef7af215779e2534
Demon4ort <dannylkodem@gmail.com>
2021-04-18 19:29:28 +0300

    deleted images added password check

--- users/forms.py
+++ users/forms.py
@@ -7,16 +7,23 @@ GENDER_CHOICES = (
 
 
 class UserForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
     first_name = forms.CharField(label='First Name', max_length=100)
     second_name = forms.CharField(label='Last Name', max_length=100)
-    photo = forms.ImageField(label='Photo')
     email = forms.EmailField(label='Email')
     facebook = forms.URLField(label='Facebook')
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
     c_password = forms.CharField(label='Confirm password', widget=forms.PasswordInput())
     date_of_birth = forms.DateField(label='Date of birth')
     phone = forms.CharField(label='Phone number', max_length=10)
-    gender=forms.ChoiceField(label='Choose your gender', choices=GENDER_CHOICES)
+    gender = forms.ChoiceField(label='Choose your gender', choices=GENDER_CHOICES)
 
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        c_password = cleaned_data.get("c_password")
 
+        if password != c_password:
+            self.add_error('c_password', "Password does not match")
 
+        return cleaned_data
--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
+from django.contrib import messages
 
 
 def register(request):
-    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('index')
+    else:
+        form = UserForm()
     return render(request, 'registration_page.html', {'form': form})
 
 
